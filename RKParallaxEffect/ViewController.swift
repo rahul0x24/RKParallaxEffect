78ed3ac58ce6bff7b431cd74cec244c02291698d
Rahul Katariya <rahulkatariya@me.com>
2015-02-02 11:42:39 +0530

    Initial Commit

---
+++ RKParallaxEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RKParallaxEffect
+//
+//  Created by Rahul Katariya on 02/02/15.
+//  Copyright (c) 2015 Rahul Katariya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
