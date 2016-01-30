7dc3db480ee6ab37b2faad47b4bd03b8f702a41f
Rahul Katariya <rahulkatariya@me.com>
2016-01-30 16:30:45 +0530

    Add Framework

---
+++ RKParallaxEffect/RKParallaxEffect/RKParallaxEffect.h
@@ -0,0 +1,19 @@
+//
+//  RKParallaxEffect.h
+//  RKParallaxEffect
+//
+//  Created by Rahul Katariya on 30/01/16.
+//  Copyright © 2016 Rahul Katariya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKParallaxEffect.
+FOUNDATION_EXPORT double RKParallaxEffectVersionNumber;
+
+//! Project version string for RKParallaxEffect.
+FOUNDATION_EXPORT const unsigned char RKParallaxEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKParallaxEffect/PublicHeader.h>
+
+
